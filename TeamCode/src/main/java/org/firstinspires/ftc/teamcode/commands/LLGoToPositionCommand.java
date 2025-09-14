d60ff645a23cd422c82092c4b65cb6cb6be91733
Abhi-karthi <164704465+Abhi-karthi@users.noreply.github.com>
2025-09-13 21:15:57 -0500

    added LLGoToPositionCommand.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/commands/LLGoToPositionCommand.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/commands/LLGoToPositionCommand.java
@@ -1,11 +1,25 @@
 package org.firstinspires.ftc.teamcode.commands;
 
+import com.qualcomm.robotcore.hardware.Gamepad;
 import com.qualcomm.robotcore.robocol.Command;
 import com.seattlesolvers.solverslib.command.CommandBase;
 
 import org.firstinspires.ftc.teamcode.subsystems.LimelightSubsystem;
+import org.firstinspires.ftc.teamcode.subsystems.MecanumDriveSubsystem;
 
 public class LLGoToPositionCommand extends CommandBase {
     LimelightSubsystem ll;
-    public
+    final private double xTarget;
+    final private double yTarget;
+    private final MecanumDriveSubsystem drive;
+    private final Gamepad gamepad;
+    //    final private double
+    public LLGoToPositionCommand(Gamepad gamepad, MecanumDriveSubsystem drive, double xTarget, double yTarget) {
+        this.xTarget = xTarget;
+        this.yTarget = yTarget;
+        this.drive = drive;
+        this.gamepad = gamepad;
+    }
+
+
 }
