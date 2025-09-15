52799f76fe3e07465f62eb63a297611ae04d6752
Abhi-karthi <karthiicode@icloud.com>
2025-09-14 20:34:44 -0500

    added teams and align and go to position to teleop

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/game/TeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/game/TeleOp.java
@@ -17,23 +17,34 @@ public class TeleOp extends OpMode {
     private DriveCommand driveCommand;
     private LLAlignCommand LLAlignCommand;
     private LLGoToPositionCommand LLGoToPositionCommand;
+    public final double[] redBaseCenterCoordinates = new double[]{0.9914, -0.8386}; // coords x,y in meters
+    public final double[] blueBaseCenterCoordinates = new double[]{0.9914, 0.8386};
+    public final double goToPositionPositionWithLLTolerance = 0.02;
+    private String team;
 
     public void init() {
         driveSubsystem = new MecanumDriveSubsystem(hardwareMap);
         driveCommand = new DriveCommand(gamepad1, driveSubsystem);
         CommandScheduler.getInstance().schedule(driveCommand);
+
+        if (team.isEmpty()) {
+            if (gamepad1.right_bumper) team = "red";
+            if (gamepad1.left_bumper) team = "blue";
+        }
+    }
+    public void init_loop() {}
+    public void start() {}
+    public void loop() {
         if (gamepad1.a) {
             LLAlignCommand = new LLAlignCommand(driveSubsystem);
             CommandScheduler.getInstance().schedule(LLAlignCommand);
         }
 
-//        if (gamepad1.x) {
-//            LLGoToPositionCommand = new LLGoToPositionCommand(gamepad1, );
-//        }
-    }
-    public void init_loop() {}
-    public void start() {}
-    public void loop() {
+        if (gamepad1.x) {
+            if (team == "blue") LLGoToPositionCommand = new LLGoToPositionCommand(driveSubsystem, blueBaseCenterCoordinates[0], blueBaseCenterCoordinates[1], goToPositionPositionWithLLTolerance);
+            if (team == "red") LLGoToPositionCommand = new LLGoToPositionCommand(driveSubsystem, redBaseCenterCoordinates[0], redBaseCenterCoordinates[1], goToPositionPositionWithLLTolerance);
+            CommandScheduler.getInstance().schedule(LLGoToPositionCommand);
+        }
         CommandScheduler.getInstance().run();
     }
     public void stop() {
