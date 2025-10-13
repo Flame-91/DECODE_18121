2806e977179c01400736b9062fe25f90a61e1223
Abhi-karthi <164704465+Abhi-karthi@users.noreply.github.com>
2025-10-13 13:27:54 -0500

    updated PPR value

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/ConfigConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/ConfigConstants.java
@@ -11,6 +11,4 @@ public class ConfigConstants {
     public static double pivotKP = 0.1;
     public static double pivotKI = 0.01;
     public static double pivotKD = 0.0001;
-    public static final double encoderTicksM = 5;  //Not necessary to be stored as a variable as it is a constant only used in this class
-    public static final double encoderTicksB = 5;
 }
