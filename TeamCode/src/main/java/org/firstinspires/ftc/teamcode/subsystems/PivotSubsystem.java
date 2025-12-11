e6a795eb09a976b275d5920cd848a92638c1a86a
Abhi-karthi <karthiicode@icloud.com>
2025-12-10 20:11:12 -0600

    Redid ticks to angle and angle to ticks calculation

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/PivotSubsystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/PivotSubsystem.java
@@ -1,5 +1,8 @@
 package org.firstinspires.ftc.teamcode.subsystems;
 
+import static org.firstinspires.ftc.teamcode.util.GlobalConstants.pivotTicksAtFortyFive;
+import static org.firstinspires.ftc.teamcode.util.GlobalConstants.pivotTicksAtNinety;
+
 import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
@@ -31,10 +34,12 @@ public class PivotSubsystem extends SubsystemBase {
     @Override
     public void periodic() {
         telemetry.addData("pivotPower", getPivotPower());
-        telemetry.addData("currentPivotPosition", getCurrentPivotPosition());
+        telemetry.addData("currentPivotPositionAngle", getCurrentPivotPositionAngle());
+        telemetry.addData("currentPivotPositionTicks", getCurrentPivotPositionTicks());
 
         telemetryPacket.put("pivotPower", getPivotPower());
-        telemetryPacket.put("currentPivotPosition", getCurrentPivotPosition());
+        telemetryPacket.put("currentPivotPositionAngle", getCurrentPivotPositionAngle());
+        telemetryPacket.put("currentPivotPositionTicks", getCurrentPivotPositionTicks());
     }
 
     public void setPivotPower(double power) {
@@ -43,7 +48,7 @@ public class PivotSubsystem extends SubsystemBase {
     }
 
     public double convertPivotTicksToAngle(double ticks) {
-        return ticks * (360/384.5); // 384.5 is PPR of motor (resolution of encoder) and this value depends on which motor we r using but the PPR is available on GoBilda.com
+        return (int) ((int) ticks*(45 / (pivotTicksAtNinety-pivotTicksAtFortyFive)));
     }
     public void movePivotWithoutEncoder(double power) {
         leftPivotMotor.setPower(power);
@@ -56,10 +61,11 @@ public class PivotSubsystem extends SubsystemBase {
         rightPivotMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
     }
     public int convertPivotAngleToTicks(double angle) {
-        return (int) (angle * (384.5/360));
+        return (int) ((int) angle*((pivotTicksAtNinety-pivotTicksAtFortyFive) / 45));
     }
 
-    public double getCurrentPivotPosition() { return leftPivotMotor.getCurrentPosition()  - convertPivotAngleToTicks(offsetAngleFromLimelightToPivot); }
-//    public double getTargetPivotPosition() { return leftPivotMotor.getTargetPosition()  - convertPivotAngleToTicks(offsetAngleFromLimelightToPivot); }
+    public double getCurrentPivotPositionTicks() { return leftPivotMotor.getCurrentPosition()  - convertPivotAngleToTicks(offsetAngleFromLimelightToPivot); }
+    public double getCurrentPivotPositionAngle() { return convertPivotTicksToAngle(leftPivotMotor.getCurrentPosition()) - offsetAngleFromLimelightToPivot; }
+    //    public double getTargetPivotPosition() { return leftPivotMotor.getTargetPosition()  - convertPivotAngleToTicks(offsetAngleFromLimelightToPivot); }
     public double getPivotPower() { return leftPivotMotor.getPower(); }
 }
