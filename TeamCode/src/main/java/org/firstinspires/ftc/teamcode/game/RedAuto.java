28cdfb1570e1bb744a08f106ae6c3d45413c40f2
Abhi-karthi <164704465+Abhi-karthi@users.noreply.github.com>
2025-12-12 09:02:16 -0600

    fixed important pivot pid error

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/game/RedAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/game/RedAuto.java
@@ -333,7 +333,7 @@ public class RedAuto extends OpMode {
     public void pivotUpdate() {
         if (limelightSubsystem.hasTarget()) {
             double pivotPositionAngle = pivotSubsystem.convertPivotTicksToAngle(pivotSubsystem.getCurrentPivotPositionAngle());
-            double targetPosition = limelightSubsystem.getPitchError(0.42545);
+            double targetPosition = limelightSubsystem.getPitchErrorDegrees(0.42545);
             double pitchError = targetPosition - pivotPositionAngle; // 0.42545 is how far up from the center of the april tag we need to shoot
 
             long currentTime = System.nanoTime();
